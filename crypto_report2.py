cbb313b00ee745ce4fae253f8402165fcd47b711
Jaida Kenana <jaidakenana@mac.mynetworksettings.com>
2025-04-23 17:19:23 -0400

    increased coingecko api sleep times

--- crypto_report2.py
+++ crypto_report2.py
@@ -149,10 +149,10 @@ def get_crypto_news():
 # --- REPORT GENERATION ---
 def generate_report():
     market_data = get_market_data()
-    time.sleep(1)
+    time.sleep(2)
 
     market_caps = get_market_caps()
-    time.sleep(1)
+    time.sleep(2)
 
     defillama_data = get_defillama_data()
 
