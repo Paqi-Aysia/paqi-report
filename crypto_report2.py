735d2128d39c68651bd0d84dc90ad13700e0aab0
Jaida Kenana <jaidakenana@mac.mynetworksettings.com>
2025-04-23 21:14:05 -0400

    Decreased coingecko tokens checked per page from 500 to 250 for rate limitations

--- crypto_report2.py
+++ crypto_report2.py
@@ -23,7 +23,7 @@ def get_market_data():
     params = {
         "vs_currency": "usd",
         "order": "market_cap_desc",
-        "per_page": 500,
+        "per_page": 250, #checks only top movers for top 250 tokens
         "page": 1,
         "price_change_percentage": "24h",
     }
