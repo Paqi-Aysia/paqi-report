4b8c82915ebbe1c28748d75124e51b5d7e1c5a8c
Jaida Kenana <jaidakenana@mac.mynetworksettings.com>
2025-04-23 21:55:28 -0400

    increased coingecko api sleep times

--- crypto_report2.py
+++ crypto_report2.py
@@ -19,7 +19,7 @@ logging.basicConfig(level=logging.INFO)
 # --- API FETCHING FUNCTIONS ---
 
 def get_market_data():
-    time.sleep(2)
+    time.sleep(3)
     url = "https://api.coingecko.com/api/v3/coins/markets"
     params = {
         "vs_currency": "usd",
@@ -31,7 +31,7 @@ def get_market_data():
     return fetch_json(url, params) or []
 
 def get_market_caps():
-    time.sleep(2)
+    time.sleep(3)
     url = "https://api.coingecko.com/api/v3/coins/categories"
     data = fetch_json(url)
 
@@ -147,7 +147,7 @@ def get_crypto_news():
 
 # --- REPORT GENERATION ---
 def generate_report():
-    time.sleep(4)
+    time.sleep(8)
     market_data = get_market_data()
 
     time.sleep(8)
